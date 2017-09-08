34062034cd8ba4499566d8d979e3df628f09f2fd
Jan Harders <jan@delinquent.de>
2017-09-08 23:18:10 +0200

    remove multiple <link> nodes

--- src/content.js
+++ src/content.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var elOpenSearch = document.querySelector('[type="application/opensearchdescription+xml"]');
-  if(elOpenSearch) {
+  var elOpenSearch;
+  while(elOpenSearch = document.querySelector('[type="application/opensearchdescription+xml"]')) {
     elOpenSearch.remove();
     chrome.runtime.sendMessage("opensearch-block");
   }  
