afba19a290cd6885cab8c4c238c76028104fe48d
Procyon-b <40043075+Procyon-b@users.noreply.github.com>
2018-06-24 16:48:00 -0400

    DOMContentLoaded/load timer; name=""
    Fixes for #14 - as zipped at
    https://github.com/gregsadetsky/chrome-dont-add-custom-search-engines/issues/14#issuecomment-399790728

--- src/content.js
+++ src/content.js
@@ -1,11 +1,27 @@
-const DEBUG=false;
+const DEBUG=true;
 let numseen=0, numspoiled=0;
 let unspoiled=[];
+
+// called when the user clicks an element of the form (any field or button). The parameter passed if the event object
+function clickApply(e) {
+  if(DEBUG) console.info({'form onclick':e});
+  // remove onclick. One fix only
+  e.srcElement.form.removeEventListener("click", clickApply);
+  applyFix(e.srcElement.form);
+}
+
+// add a new <textarea> element
+function applyFix(elem) {
+  var newelem = document.createElement('textarea');
+  newelem.name = '';
+  newelem.style.display='none';
+  elem.appendChild(newelem);
+}
 
 // Add an extra child input to any form that only has one
 function spoilFormGet(elem) {
     if(DEBUG) {
-        console.info({Found: elem});
+        console.info({t:this, Found: elem});
         ++numseen;
         unspoiled.push(elem);
     }
@@ -27,11 +43,15 @@ function spoilFormGet(elem) {
 
     // Add a <textarea> - unlike <input>, it doesn't block implicit submission
     // per https://www.tjvantoll.com/2013/01/01/enter-should-submit-forms-stop-messing-with-that/
-    var newelem;
-    newelem = document.createElement('textarea');
-    newelem.name = 'chrome_dont_add_custom_search_engines_srsly';
-    newelem.style.display='none';
-    elem.appendChild(newelem);
+
+    // apply the fix now, or place it in onclick.  "this" is a parameter passed by foreach(). see below
+    if (this == true) {
+      // remove onclick placed during first pass
+      elem.removeEventListener("click", clickApply);
+      // and instead do it now;
+      applyFix(elem);
+      }
+    else elem.addEventListener('click', clickApply);
 
     if(DEBUG) {
         console.info({Spoiled: elem});
@@ -39,6 +59,22 @@ function spoilFormGet(elem) {
         unspoiled.pop();
     }
 } //spoilFormGet
+
+var debugAutoDetect=0
+// move this part of the code here, since it's called 3 times
+function autoDetect(now,cmt) {
+    if(DEBUG) console.log('autoDetect: '+(++debugAutoDetect)+' ('+cmt+')')
+    document.querySelectorAll('form:-webkit-any([method="get" i],:not([method]))').forEach(spoilFormGet,now);
+    if(DEBUG) {
+        console.log(`Spoiled ${numspoiled}/${numseen}.  Unspoiled were:`);
+        console.log(unspoiled);
+    }
+
+    // we reset spoil vars for next call
+    numseen=0;
+    numspoiled=0;
+    unspoiled=[];
+}
 
 function main() {   // runs on DOMContentLoaded
 
@@ -59,16 +95,24 @@ function main() {   // runs on DOMContentLoaded
         }
     );
 
-    // Chrome autodetection, https://www.chromium.org/tab-to-search #2 .
-    window.addEventListener('load', function() {
-        document.querySelectorAll('form:-webkit-any([method="get" i],:not([method]))').forEach(spoilFormGet);
+    // if URL has a path, no autodetection will be performed. So only perform "spoil" when path is "/"
+    // Chrome autodetection, https://www.chromium.org/tab-to-search #2
 
-        if(DEBUG) {
-            console.log(`Spoiled ${numspoiled}/${numseen}.  Unspoiled were:`);
-            console.log(unspoiled);
-        }
-    });
+    // #1 call it now without applying the fix
+    // #2 call it in 1500 ms  and applies the fix
+    // #3 call when document loaded, and applies the fix
+    // if <form> are added/modified dynamically before the document is fully loaded #1 could miss it,
+    // but not #2 & #3. Note that #2 could fire after #3 if the page is fast to load.
+    // Once the fix is applied, the <form> can't be found by subsequent execution ot autoDetect,
+    // so the fix can only be applied once (#1 is not applied but delayed until #2 or #3 fires, or if the user clicks).
+
+    window.addEventListener('load', function() { autoDetect(true,'Load'); } );
+    setTimeout(function() { autoDetect(true,'Timer'); } ,1500);
+    autoDetect(false,'onClick');
+
 } //main
+
+var cc=0;
 
 document.addEventListener('DOMContentLoaded', main);
 
