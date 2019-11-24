60ed9fa2ac46ff87654c6a8837ffaa7e181f3744
Chris White <cxwembedded@gmail.com>
2019-11-23 19:56:11 -0500

    Beautify - whitespace changes only
    No functional changes.  Just ran everything through beautify.sh.

--- src/background.js
+++ src/background.js
@@ -1,70 +1,81 @@
 "use strict";
 
-var defSitesWMut={
-  'www.nytimes.com': ['.css-10488qs'],
-  'www.brico.be': ['.mxd-search-initial', 'pN', 'pN']
-  };
-var sitesWMut={};
+var defSitesWMut = {
+    'www.nytimes.com': ['.css-10488qs'],
+    'www.brico.be': ['.mxd-search-initial', 'pN', 'pN']
+};
+var sitesWMut = {};
 
-var expire=90000;
+var expire = 90000;
 
 // parse stored data (string) and populate array
 // "text" is used when called from the options page (regenerates string version of data with valid syntax, and keeps comments) 
-function toAr(v,text) {
-  var r={}, t={}, n, RE=/^\s*(\S+)\s*(.*)$/, RE2=/^(?:"([^"]*?)"|(.+?))(?:\s+(\S.*))?\s*$/,
-    REcmt=/^(\s*\/\/.*|)$/;
+function toAr(v, text) {
+    var r = {},
+        t = {},
+        n, RE = /^\s*(\S+)\s*(.*)$/,
+        RE2 = /^(?:"([^"]*?)"|(.+?))(?:\s+(\S.*))?\s*$/,
+        REcmt = /^(\s*\/\/.*|)$/;
 
-  v=v.split('\n');
-  for (let i=0; i<v.length; i++) {
-    if (text) {
-      n='cmt'+i;
-      r[n]={txt:v[i].replace(/^\s+/,'')};
-      }
+    v = v.split('\n');
+    for (let i = 0; i < v.length; i++) {
+        if (text) {
+            n = 'cmt' + i;
+            r[n] = {
+                txt: v[i].replace(/^\s+/, '')
+            };
+        }
 
-    if (REcmt.test(v[i])) {
-      if (text) r[n].cmt=1;
-      continue;
-      }
-    if (!RE.test(v[i])) continue;
+        if (REcmt.test(v[i])) {
+            if (text) r[n].cmt = 1;
+            continue;
+        }
+        if (!RE.test(v[i])) continue;
 
-    var k=RegExp.$1;
+        var k = RegExp.$1;
 
-    if (text && r[k]) {
-      r[ r[k].n ].cmt=1;
-      }
+        if (text && r[k]) {
+            r[r[k].n].cmt = 1;
+        }
 
-    if (text) {r[k]={n};r[n].k=k;}
-    else r[k]={};
+        if (text) {
+            r[k] = {
+                n
+            };
+            r[n].k = k;
+        } else r[k] = {};
 
-    if (RegExp.$2 == 'null') {
-      if (text) r[k].null=1;
-      else r[k]=0;
-      continue;
-      }
-    var a=[], j=20, s=RegExp.$2;
-    while (j-- && RE2.test(s)) {
-      s=RegExp.$3;
-      a.push(RegExp.$1 || RegExp.$2);
-      }
-    if (a[0]=='!') {
-      r[k].fb=1;
-      a.shift();
-      }
-    r[k].length=a.length;
-    Object.assign(r[k],a);
-    if (text) {
-      Object.assign(r[n],a);
-      r[n].length=a.length;
-      }
+        if (RegExp.$2 == 'null') {
+            if (text) r[k].null = 1;
+            else r[k] = 0;
+            continue;
+        }
+        var a = [],
+            j = 20,
+            s = RegExp.$2;
+        while (j-- && RE2.test(s)) {
+            s = RegExp.$3;
+            a.push(RegExp.$1 || RegExp.$2);
+        }
+        if (a[0] == '!') {
+            r[k].fb = 1;
+            a.shift();
+        }
+        r[k].length = a.length;
+        Object.assign(r[k], a);
+        if (text) {
+            Object.assign(r[n], a);
+            r[n].length = a.length;
+        }
     }
-  return r;
+    return r;
 }
 
 function getStor() {
-  var s=toAr(localStorage.sitesWMut || '');
-  sitesWMut=JSON.parse(JSON.stringify(defSitesWMut));
-  for (var k in s) {
-    sitesWMut[k]=s[k];
+    var s = toAr(localStorage.sitesWMut || '');
+    sitesWMut = JSON.parse(JSON.stringify(defSitesWMut));
+    for (var k in s) {
+        sitesWMut[k] = s[k];
     }
 }
 
@@ -72,11 +83,11 @@ getStor();
 
 // receive messages from contentscript
 chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if (sender.id != chrome.runtime.id) return; // not from this extension
-    if (request.host) {
-      let s, r={};
-      if (s=sitesWMut[request.host]) r.siteMut=s;
-      sendResponse(r);
-      }
-  });
+    function(request, sender, sendResponse) {
+        if (sender.id != chrome.runtime.id) return; // not from this extension
+        if (request.host) {
+            let s, r = {};
+            if (s = sitesWMut[request.host]) r.siteMut = s;
+            sendResponse(r);
+        }
+    });
--- src/options.js
+++ src/options.js
@@ -1,38 +1,43 @@
 "use strict";
 
-var TA=document.getElementById('list'), b=document.getElementById('b'), def=document.getElementById('def'), bg=chrome.extension.getBackgroundPage();
+var TA = document.getElementById('list'),
+    b = document.getElementById('b'),
+    def = document.getElementById('def'),
+    bg = chrome.extension.getBackgroundPage();
 
-var LS=localStorage.sitesWMut;
+var LS = localStorage.sitesWMut;
 
-TA.value=LS;
-def.value+=toStr(bg.defSitesWMut,1);
-def.style.height=def.scrollHeight+4+'px';
+TA.value = LS;
+def.value += toStr(bg.defSitesWMut, 1);
+def.style.height = def.scrollHeight + 4 + 'px';
 
 // Convert array of values back to string. Including comments
-function toStr(a,raw) {
-  var r='', h;
-  for (let k in a) {
-    if (!raw && a[k].txt === undefined) continue;
-    if (a[k].cmt) {
-      r+=a[k].txt+'\n';
-      continue;
-      }
-    h=a[k].k || k;
-    r+=h;
-    if (a[h].null) r+=' null';
-    else for (let e,i=0; e=a[h][i]; i++) {
-      if (i==0) e=(a[h].fb?'! ':'')+'"'+e+'"';
-      r+=' '+e;
-      }
-    r+='\n'
+function toStr(a, raw) {
+    var r = '',
+        h;
+    for (let k in a) {
+        if (!raw && a[k].txt === undefined) continue;
+        if (a[k].cmt) {
+            r += a[k].txt + '\n';
+            continue;
+        }
+        h = a[k].k || k;
+        r += h;
+        if (a[h].null) r += ' null';
+        else
+            for (let e, i = 0; e = a[h][i]; i++) {
+                if (i == 0) e = (a[h].fb ? '! ' : '') + '"' + e + '"';
+                r += ' ' + e;
+            }
+        r += '\n'
     }
-  return r;
+    return r;
 }
 
 // Validate current config using the background page function - save - and reload data 
-b.onclick=function(){
-  var s=toStr( bg.toAr(TA.value, 1) ).replace(/\n+$/,'\n');
-  TA.value=s;
-  localStorage.setItem('sitesWMut', s);
-  bg.getStor();
-  };
+b.onclick = function() {
+    var s = toStr(bg.toAr(TA.value, 1)).replace(/\n+$/, '\n');
+    TA.value = s;
+    localStorage.setItem('sitesWMut', s);
+    bg.getStor();
+};
